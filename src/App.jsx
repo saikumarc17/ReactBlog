f94c1545d538c62a904f199694080902b19bab19
saikumarc17 <saikumar.c17@gmail.com>
2024-06-06 19:58:17 +0530

    project basic setup

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css';
+
+
+function App() {
+
+  console.log(import.meta.env.VITE_APPWRITE_URL)
+  console.log(import.meta.env.VITE_APPWRITE_BUCKET_ID)
+  return (
+    <h1>
+      React app gitcode
+    </h1>
+  )
+}
+
+export default App
---
+++ src/config/config.js
@@ -0,0 +1,10 @@
+const config={
+    appWriteUrl:String(import.meta.env.VITE_APPWRITE_URL),
+    appWriteProjectId:String(import.meta.env.VITE_APPWRITE_PROJECT_ID),
+    appWriteDatabaseId:String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
+    appwriteArticlesCollectionId:String(import.meta.env.VITE_APPWRITE_Articles_COLLECTION_ID),
+    appWriteBucketId:String(import.meta.env.VITE_APPWRITE_BUCKET_ID)
+
+}
+
+export default config;
